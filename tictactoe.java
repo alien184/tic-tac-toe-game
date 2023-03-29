6bd464c240921f0487a4dab587aabca81f07b78e
alien184 <106427356+alien184@users.noreply.github.com>
2023-03-29 14:10:20 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -1,78 +1,74 @@
 import java.util.Scanner;
 
-class Main{
-public static void main(String[] args) {
-    char[][] board = new char[3][3];
-    for(int row=0;row<board.length;row++){
-        for(int col=0;col<board[row].length;col++){
-            board[row][col]= ' ';
+class Main {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = ' ';
+            }
         }
-    }
 
-    char player = 'X';
-    boolean gameOver=false;
-    Scanner scanner=new Scanner(System.in);
-    
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner scanner = new Scanner(System.in);
 
-    while(!gameOver){
+        while (!gameOver) {
+            printBoard(board);
+            System.out.println("Player " + player + " enter:");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if (board[row][col] == ' ') {
+                board[row][col] = player;// place the element
+                gameOver = hasWon(board, player);
+                if (gameOver) {
+                    System.out.println("PLAYER " + player + " has won");
+                } else {
+                    player = (player == 'X') ? 'O' : 'X';
+
+                }
+            } else {
+                System.out.println("INVALID MOVE YOU PUNK");
+            }
+
+        }
         printBoard(board);
-        System.out.println("Player "+ player +"enter:");
-        int row = scanner.nextInt();
-        int col = scanner.nextInt();
 
-        if(board[row][col]==' '){
-            board[row][col]=player;//place the element
-            gameOver = hasWon(board,player);
-            if(gameOver){
-                System.out.println("PLAYER "+ player + " has won");
-            }else{
-                player=(player =='X') ? 'O' : 'X';
-
-            }
-                                }
-        else{
-            System.out.println("INVALID MOVE YOU PUNK");
-            }
-            
     }
-    printBoard(board);
 
-}
-public static void printBoard(char[][] board){
-    for(int row=0;row<board.length;row++){
-        for(int col=0;col<board[row].length;col++){
-            System.out.println(board[row][col]+ '|');
+    public static boolean hasWon(char[][] board, char player) {
+        // for row
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][0] == player && board[row][1] == player && board[row][2] == player) {
+                return true;
+            }
+        }
+        // for col
+        for (int col = 0; col < board[0].length; col++) {
+            if (board[0][col] == player && board[1][col] == player && board[2][col] == player) {
+                return true;
+            }
 
         }
-        System.out.println();
-    }
-}
-public static boolean hasWon(char[][] board ,char player){
-    //for row
-    for(int row=0;row<board.length;row++){
-        if(board[row][0]==player && board[row][1] ==player && board[row][2]==player){
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
             return true;
         }
-    }
-    //for col
-    for(int col=0;col<board[0].length;col++){
-        if(board[0][col]==player && board[1][col] ==player && board[2][col]==player){
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
             return true;
         }
-    
-    }
-    if(board[0][0]==player && board[1][1]==player && board[2][2]==player){
-        return true;
-    }
-    if(board[0][2]==player && board[1][1]==player && board[2][0]==player){
-        return true;
-    }
-    return false;
+        return false;
 
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col] + " | ");
+
+            }
+            System.out.println();
+        }
 
     }
 }
-
-
-
-
